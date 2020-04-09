36919e7e124fa5e197f4fbc7619e45121d38f977
Yunior <yunior0509av@gmail.com>
2020-04-09 16:41:32 -0400

    template part

--- wp-content/themes/best/front-page.php
+++ wp-content/themes/best/front-page.php
@@ -47,18 +47,9 @@
         $homePosts = new WP_Query(array(
           'posts_per_page' => 2
         ));
-        while($homePosts->have_posts()): $homePosts->the_post() ?>
-                  <div class="event-summary">
-                  <a class="event-summary__date event-summary__date--beige t-center" href="<?php the_permalink() ?>">
-                    <span class="event-summary__month"><?php the_time('M')?></span>
-                    <span class="event-summary__day"><?php the_time('n') ?></span>  
-                  </a>
-                  <div class="event-summary__content">
-                    <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h5>
-                    <p><?php the_excerpt() ?> <a href="<?php the_permalink() ?>" class="nu gray">Read more</a></p>
-                  </div>
-                </div>
-        <?php endwhile;?>
+        while($homePosts->have_posts()){ $homePosts->the_post();
+          get_template_part('template-parts/content-event');
+        }?>
         <p class="t-center no-margin"><a href="<?= site_url('\blog') ?>" class="btn btn--yellow">View All Blog Posts</a></p> <!-- \< ? = is a short php tag -->
       </div>
     </div>
